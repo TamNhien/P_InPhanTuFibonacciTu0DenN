d62da1fd3c356fbbdfc5ca201ff4e334e4b596a5
TamNhien <canhthien91@gmail.com>
2024-03-19 19:16:16 +0700

    Add project files.

---
+++ P_InPhanTuFibonacciTu0DenN.py
@@ -0,0 +1,11 @@
+﻿def fibonacci(n):
+    fib_list = [0, 1]
+    while fib_list[-1] < n: fib_list.append(fib_list[-1] + fib_list[-2])
+    return fib_list[:-1]
+
+def main():
+    n = int(input("Nhập n: "))
+    print("Các số Fibonacci từ 0 đến", n, "là :", fibonacci(n))
+
+if __name__ == "__main__":
+    main()
